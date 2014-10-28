dfc55e1be7c44ee97a6cda47fbf9e0d573db61f1
Douglas Hill <hello@douglashill.co>
2014-10-28 12:37:36 +0000

    Update imports and documentation in main header file.

--- DHClustering/KMClustering.h
+++ DHClustering/KMClustering.h
@@ -3,7 +3,8 @@
 
 @import Foundation;
 
+#import "KMCluster.h"
 #import "KMVector.h"
 
-/// Runs the k-means clustering algorithm until convergence or `maxIterations` is reached. The objects in both `initialMeans` and `observationVectors` must conform to the `KMVector` protocol.
+/// Runs the k-means clustering algorithm `maxIterations` times. The objects in both `initialMeans` and `observationVectors` must conform to the `KMVector` protocol. Returns a set of `KMCluster`s.
 NSSet *clustersWithInitialMeansAndObservations(NSSet *initialMeans, NSSet *observationVectors, NSUInteger maxIterations);
